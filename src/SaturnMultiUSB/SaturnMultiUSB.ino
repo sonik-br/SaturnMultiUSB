b10d62f7a8171990102b960c42fa04e756d6eafa
Matheus Fraguas <metalsonik@gmail.com>
2021-07-26 20:32:28 -0300

    Support for megadrive input
    Changed SAT1_D0 and SAT1_DA pins.
    Added support for megadrive/genesis controllers and multitap.

--- src/SaturnMultiUSB/SaturnMultiUSB.ino
+++ src/SaturnMultiUSB/SaturnMultiUSB.ino
@@ -5,7 +5,7 @@
  * 
  * By using the multitap it's possible to connect up to 7 controllers.
  * 
- * Button mapping follows the PS3 standard.
+ * Also works with MegaDrive controllers and mulltitaps.
  * 
  * It uses the ArduinoJoystickLibrary and the DigitalWriteFast Library.
  * 
@@ -23,11 +23,15 @@
 //Arduino Digital Write Fast Library
 #include <digitalWriteFast.h>
 
+//#define ENABLE_SERIAL_DEBUG
+
+#define SATURN_PORTS 2 //1 or 2 controller ports
+
 //Saturn port 1
 #define SAT1_TH A2
 #define SAT1_TR 7
-#define SAT1_D0 A5
-#define SAT1_D1 A4
+#define SAT1_D0 A4
+#define SAT1_D1 A5
 #define SAT1_D2 A3
 #define SAT1_D3 A0
 #define SAT1_TL A1
@@ -43,9 +47,10 @@
 #define SAT2_TL 5
 #define SAT2_PINGROUP PIND
 
-//#define ENABLE_SERIAL_DEBUG
+#define ENABLE_8BITDO_HOME_BTN//coment to disable the use of HOME button on 8bidto M30 2.4G.
 
-#define MAX_USB_STICKS 7
+
+#define MAX_USB_STICKS 5 + SATURN_PORTS //maximum 7 controllers per arduino
 
 #ifdef ENABLE_SERIAL_DEBUG
 #define dstart(spd) do {Serial.begin (spd); while (!Serial) {digitalWrite (LED_BUILTIN, (millis () / 500) % 2);}} while (0);
@@ -64,7 +69,15 @@ uint8_t lastState[MAX_USB_STICKS][8]= {}; //Store the state of each controller
 
 uint8_t joyStateChanged = B00000000; //Store if each joystick has changed state
 
-short totalUsb = 2;
+enum DB9_TR_Enum {
+  DB9_TR_INPUT = 0,
+  DB9_TR_OUTPUT
+};
+
+DB9_TR_Enum portState[2] = { DB9_TR_INPUT, DB9_TR_INPUT };
+
+
+short totalUsb = SATURN_PORTS;
 short joyCount = 0;
 short currentPort = 1;
 int16_t hatTable[16];
@@ -134,6 +147,26 @@ char waitTL(char state) {
   return 0;
 }
 
+char readMegadriveBits() { //6 bits
+  bool TL;
+  bool TR;
+  uint8_t nibble = readNibble();
+
+  if(portState[currentPort-1] != DB9_TR_OUTPUT) {//saturn dont need these values
+    if (currentPort == 1) {
+      TL = digitalReadFast(SAT1_TL);
+      TR = digitalReadFast(SAT1_TR);
+    } else {
+      TL = digitalReadFast(SAT2_TL);
+      TR = digitalReadFast(SAT2_TR);
+    }
+  
+    bitWrite(nibble, 4, TL);
+    bitWrite(nibble, 5, TR);
+  }        
+  return nibble;
+}
+
 char readNibble() {
   //Read from port 1 or port 2
   uint8_t nibble = currentPort == 1 ? SAT1_PINGROUP : SAT2_PINGROUP;
@@ -143,37 +176,150 @@ char readNibble() {
          ((nibble >> 1) & B00000001);  // D0
 }
 
+void setTR_Mode(DB9_TR_Enum mode) {
+  if(portState[currentPort-1] != mode) {
+    portState[currentPort-1] = mode;
+    if(mode == DB9_TR_OUTPUT) {
+      if (currentPort == 1)
+        pinMode(SAT1_TR, OUTPUT);
+      else
+        pinMode(SAT2_TR, OUTPUT);
+      setTR_High();
+    } else {
+      if (currentPort == 1)
+        pinMode(SAT1_TR, INPUT_PULLUP);
+      else
+        pinMode(SAT2_TR, INPUT_PULLUP);
+    }
+  }
+}
+
 void readSatPort() {
   uint8_t nibble_0;
   uint8_t nibble_1;
 
   setTH_High();
-  setTR_High();
   delayMicroseconds(4);
+  nibble_0 = readMegadriveBits();
 
-  //if (digitalRead(SAT1_TL) == LOW) //Device is not ready
-    //return;
+  setTH_Low();
+  delayMicroseconds(4);
+  nibble_1 = readMegadriveBits();
 
-  nibble_0 = readNibble();
-
-  if ((nibble_0 & B00000111) == B00000100) {
+  if ((nibble_0 & B00000111) == B00000100) { //Saturn ID
     //debugln (F("DIGITAL-PAD"));
-    readDigitalPad(nibble_0);
+    setTR_Mode(DB9_TR_OUTPUT);
+    readDigitalPad(nibble_0 & B00001111, nibble_1 & B00001111);
+  } else if ((nibble_0 & B00001111) == B00000001 && (nibble_1 & B00001111) == B00000001) { //Saturn ID
+    //debugln (F("3-WIRE-HANDSHAKE"));
+    setTR_Mode(DB9_TR_OUTPUT);
+    readThreeWire();
+  } else if((nibble_1 & B00001100) == B00000000) { //Megadrive ID
+    //debugln (F("MEGADRIVE"));
+    setTR_Mode(DB9_TR_INPUT);
+    readMegadrivePad(nibble_0, nibble_1);
+  } else if ((nibble_0 & B00001111) == B00000011 && (nibble_1 & B00001111) == B00001111) { //Megadrive multitap
+    setTR_Mode(DB9_TR_OUTPUT);
+    //debugln (F("MEGADRIVE MULTITAP"));
+    readMegaMultiTap();
   } else {
-    setTH_Low();
-    delayMicroseconds(4);
-
-    nibble_1 = readNibble();
-
-    if (nibble_0 == B00000001 && nibble_1 == B00000001) {
-      //debugln (F("3-WIRE-HANDSHAKE"));
-      readThreeWire();
-    }
+    setTR_Mode(DB9_TR_INPUT);
   }
 
   setTH_High();
-  setTR_High();
+
+  if (portState[currentPort-1] == DB9_TR_OUTPUT)
+    setTR_High();
+  
   delayMicroseconds(100);
+}
+
+void readMegaMultiTap() {
+  byte joyIndex;
+  uint8_t nibble_0;
+  uint8_t nibble_1;
+  uint16_t temp;
+  uint8_t nibbles;
+  char tr = 0;
+  char r;
+  int i;
+
+  //read first two nibbles. should be zero and zero
+  for (int i = 0; i < 2; i++) {
+    if (tr) {
+      setTR_High();
+      r = waitTL(1);
+      if (r)
+        return;
+    } else {
+      setTR_Low();
+      r = waitTL(0);
+      if (r)
+        return;
+    }
+
+    delayMicroseconds(4);
+
+    tr ^= 1;
+  }
+
+  //read each port controller ID and stores the 4 values in the 16 bits variable
+  for (i = 0; i < 16; i+=4) {
+    if (tr) {
+      setTR_High();
+      r = waitTL(1);
+      if (r)
+        return;
+    } else {
+      setTR_Low();
+      r = waitTL(0);
+      if (r)
+        return;
+    }
+
+    delayMicroseconds(4);
+    temp |= (readNibble() << i);
+
+    tr ^= 1;
+  }
+
+  //now read each port
+  for (i = 0; i < 16; i+=4) {
+    nibble_0 = (temp >> i) & B1111;
+
+    if (nibble_0 == B0000) //3 button. 2 nibbles
+      nibbles = 2;
+    else if (nibble_0 == B0001) //6 buttons. 3 nibbles
+      nibbles = 3;
+    else if (nibble_0 == B0010) //mouse. 6 nibbles
+      nibbles = 6;
+    else //non-connection. 0 reads
+      continue;
+
+    if (nibbles != 6)//ignore mouse
+      joyIndex = joyCount++;
+    
+    for(int x = 0; x < nibbles; x++) {
+      if (tr) {
+        setTR_High();
+        r = waitTL(1);
+        if (r)
+          return;
+      } else {
+        setTR_Low();
+        r = waitTL(0);
+        if (r)
+          return;
+      }
+      
+      if (nibbles != 6)//ignore mouse
+        setUsbValues(joyIndex, false, x, readNibble());
+      
+      tr ^= 1;
+    }
+    
+    delayMicroseconds(40);
+  }
 }
 
 void readThreeWire() {
@@ -213,14 +359,15 @@ void readThreeWire() {
   if (nibble_0 == B00000100 && nibble_1 == B00000001) {
     //debugln (F("6P MULTI-TAP"));
     readMultitap();
-  }
-  else if (nibble_0 == B00000000 && nibble_1 == B00000010) {
+  } else if (nibble_0 == B00000000 && nibble_1 == B00000010) {
     //debugln (F("DIGITAL"));
     readThreeWireController(false);
-  }
-  else if (nibble_0 == B00000001 && nibble_1 == B00000110) {
+  } else if (nibble_0 == B00000001 && nibble_1 == B00000110) {
     //debugln (F("ANALOG"));
     readThreeWireController(true);
+  } else if (nibble_0 == B00001110 && nibble_1 < B00000011) { //megadrive 3 or 6 button pad. ignore mouse
+    //debugln (F("MEGADRIVE"));
+    readThreeWireController(false);
   } else if (nibble_0 == B00001111 && nibble_1 == B00001111) {
     //debugln (F("NONE"));
   } else {
@@ -324,7 +471,7 @@ void setUsbValues(int joyIndex, bool isAnalog, int page, uint8_t nibbleTMP) {
     usbStick[joyIndex]->setButton(1, !(nibbleTMP & B00000100)); //A
     usbStick[joyIndex]->setButton(2, !(nibbleTMP & B00000001)); //B
     usbStick[joyIndex]->setButton(5, !(nibbleTMP & B00000010)); //C
-    usbStick[joyIndex]->setButton(8, !(nibbleTMP & B00001000)); //S
+    usbStick[joyIndex]->setButton(9, !(nibbleTMP & B00001000)); //S
   } else if (page == 2) { //RXYZ
     usbStick[joyIndex]->setButton(0, !(nibbleTMP & B00000100)); //X
     usbStick[joyIndex]->setButton(3, !(nibbleTMP & B00000010)); //Y
@@ -406,22 +553,67 @@ void readThreeWireController(bool isAnalog) {
   }
 }
 
-void readDigitalPad(uint8_t firstNibble) {
+void readMegadrivePad(uint8_t nibble_0, uint8_t nibble_1) {
+  byte joyIndex = joyCount++;
+
+  // RLDU
+  setUsbValues(joyIndex, false, 0, nibble_0 & B00001111);
+
+  // SACB
+  setUsbValues(joyIndex, false, 1, (nibble_0 >> 4) | (nibble_1 >> 2));
+
+  //check if is 6-button pad;
+  setTH_High();
+  delayMicroseconds(4);
+  setTH_Low();
+  delayMicroseconds(4);
+
+  setTH_High();
+  delayMicroseconds(4);
+  setTH_Low();
+  delayMicroseconds(4);
+  
+  nibble_0 = readMegadriveBits();
+  
+  if ((nibble_0 & B00001111) == B0) { //it is a 6-button pad
+    setTH_High();
+    delayMicroseconds(4);
+    nibble_0 = readMegadriveBits();
+
+    setTH_Low();
+    delayMicroseconds(4);
+    nibble_1 = readMegadriveBits();
+
+    //11MXYZ
+    setUsbValues(joyIndex, false, 2, nibble_0 & B00001111);
+
+    //SA1111
+    //8bitdo M30 extra buttons
+    //111S1H STAR and HOME
+    
+    //use HOME button as the missing L from saturn
+#ifdef ENABLE_8BITDO_HOME_BTN
+    setUsbValues(joyIndex, false, 3, nibble_1 << 3);
+#endif
+  }
+}
+
+void readDigitalPad(uint8_t nibble_0, uint8_t nibble_1) {
   byte joyIndex = joyCount++;
   uint8_t nibbleTMP;
   //uint8_t currentHatState;
 
   // L100
   //debugln (firstNibble, BIN);
-  setUsbValues(joyIndex, false, 3, firstNibble);
+  setUsbValues(joyIndex, false, 3, nibble_0);
 
   // RLDU
-  setTH_Low();
-  setTR_High();
-  delayMicroseconds(4);
-  nibbleTMP = readNibble();
-  //debugln (nibbleTMP, BIN);
-  setUsbValues(joyIndex, false, 0, nibbleTMP);
+  //setTH_Low();
+  //setTR_High();
+  //delayMicroseconds(4);
+  //nibbleTMP = readNibble();
+  //debugln (nibble_1, BIN);
+  setUsbValues(joyIndex, false, 0, nibble_1);
 
   // SACB
   setTH_High();
@@ -464,71 +656,71 @@ void detectMultitap() {
   //setTR_High();
   //delayMicroseconds(4);
 
-  r = waitTL(1); //Device is not ready yet
+  r = waitTL(1); //Device is not ready yet. Saturn and Mega multitap hold TL high while initializing.
   if (r)
     return;
 
+  //readMegadriveBits
   nibble_0 = readNibble();
 
-  if (nibble_0 == B00000001) {
-    setTH_Low();
-    delayMicroseconds(4);
+  setTH_Low();
+  delayMicroseconds(4);
+  nibble_1 = readNibble();
 
-    nibble_1 = readNibble();
-
-    if (nibble_1 == B00000001) { //3-WIRE-HANDSHAKE
-      for (i = 0; i < nibbles; i++) {
-        if (tr) {
-          setTR_High();
-          r = waitTL(1);
-          if (r)
-            return;
-        } else {
-          setTR_Low();
-          r = waitTL(0);
-          if (r)
-            return;
-        }
-        delayMicroseconds(4);
-
-        nibbleTMP = readNibble();
-
-        if (i == 0)
-          nibble_0 = nibbleTMP;
-        else
-          nibble_1 = nibbleTMP;
-
-        tr ^= 1;
+  if ((nibble_0 & B00001111) == B00000011 && (nibble_1 & B00001111) == B00001111) { //Megadrive multitap
+    setTR_Mode(DB9_TR_OUTPUT);
+    totalUsb += 3;
+  } else if (nibble_0 == B00000001 && nibble_1 == B00000001) { //SATURN 3-WIRE-HANDSHAKE
+    setTR_Mode(DB9_TR_OUTPUT);
+    for (i = 0; i < nibbles; i++) {
+      if (tr) {
+        setTR_High();
+        r = waitTL(1);
+        if (r)
+          return;
+      } else {
+        setTR_Low();
+        r = waitTL(0);
+        if (r)
+          return;
       }
+      delayMicroseconds(4);
 
-      if (nibble_0 == B00000100 && nibble_1 == B00000001) //Is a multitap
-        totalUsb += 5;
+      nibbleTMP = readNibble();
+
+      if (i == 0)
+        nibble_0 = nibbleTMP;
+      else
+        nibble_1 = nibbleTMP;
+
+      tr ^= 1;
     }
+
+    if (nibble_0 == B00000100 && nibble_1 == B00000001) //Is a multitap
+      totalUsb += 5;
   }
 
   //reset TH and TR to high
   setTH_High();
-  setTR_High();
+  
+  if (portState[currentPort-1] == DB9_TR_OUTPUT)
+    setTR_High();
+
   delayMicroseconds(4);
 }
-
 
 void setup() {
   int i;
 
   //init onboard led pin
   pinMode(LED_BUILTIN, OUTPUT);
-  
+
+
+  //Port 1 pins
+
   //init output pins
   pinMode(SAT1_TH, OUTPUT);
-  pinMode(SAT1_TR, OUTPUT);
   digitalWrite(SAT1_TH, HIGH);
-  digitalWrite(SAT1_TR, HIGH);
-
-  pinMode(SAT2_TH, OUTPUT);
-  pinMode(SAT2_TR, OUTPUT);
-  digitalWrite(SAT2_TH, HIGH);
-  digitalWrite(SAT2_TR, HIGH);
 
   //init input pins
   pinMode(SAT1_D0, INPUT_PULLUP);
@@ -537,11 +729,24 @@ void setup() {
   pinMode(SAT1_D3, INPUT_PULLUP);
   pinMode(SAT1_TL, INPUT_PULLUP);
 
+  //init TR pin. Can change to input or output during execution. Defaults to input to support megadrive controllers
+  pinMode(SAT1_TR, INPUT_PULLUP);
+
+
+  //Port 2 pins
+#if SATURN_PORTS > 1
+  pinMode(SAT2_TH, OUTPUT);
+  digitalWrite(SAT2_TH, HIGH);
+
   pinMode(SAT2_D0, INPUT_PULLUP);
   pinMode(SAT2_D1, INPUT_PULLUP);
   pinMode(SAT2_D2, INPUT_PULLUP);
   pinMode(SAT2_D3, INPUT_PULLUP);
   pinMode(SAT2_TL, INPUT_PULLUP);
+
+  pinMode(SAT2_TR, INPUT_PULLUP);
+#endif
+
 
   //hat table angles. ----RLDU
   hatTable[B00001111] = JOYSTICK_HATSWITCH_RELEASE;
@@ -560,11 +765,11 @@ void setup() {
 
   delayMicroseconds(10);
 
-  currentPort = 1;
-  detectMultitap();
-
-  currentPort = 2;
-  detectMultitap();
+  //detect multitap on ports
+  for (i = 1; i <= SATURN_PORTS; i++) {
+    currentPort = i;
+    detectMultitap();
+  }
 
   //limit to 7 usb controllers
   if (totalUsb > MAX_USB_STICKS)
@@ -575,7 +780,7 @@ void setup() {
     usbStick[i] = new Joystick_ (
       JOYSTICK_DEFAULT_REPORT_ID + i,
       JOYSTICK_TYPE_GAMEPAD,
-      9,      // buttonCount
+      10,     // buttonCount
       1,      // hatSwitchCount (0-2)
       true,   // includeXAxis
       true,   // includeYAxis
@@ -605,22 +810,22 @@ void setup() {
     usbStick[i]->setBrake(0);
     usbStick[i]->sendState();
   }
+  
+  delay(50);
 
   dstart (115200);
-  debugln (F("Powered on!"));
+  //debugln (F("Powered on!"));
 }
 
 void loop() {
   start = micros();
   joyCount = 0;
 
-  //read first saturn port
-  currentPort = 1;
-  readSatPort();
-
-  //read second saturn port
-  currentPort = 2;
-  readSatPort();
+  //read saturn ports
+  for (int i = 1; i <= SATURN_PORTS; i++) {
+    currentPort = i;
+    readSatPort();
+  }
 
   //report usb hid
   reportUsb();
